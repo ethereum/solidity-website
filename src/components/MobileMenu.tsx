b7b1fc734d46692a5d8ebc143eb01d678faecf43
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-06-01 16:11:25 -0700

    add socials to mobile menu

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -4,16 +4,17 @@ import {
   DrawerOverlay,
   DrawerContent,
   DrawerCloseButton,
+  Flex,
+  Grid,
   IconButton,
   Image,
   useDisclosure,
-  Flex,
 } from '@chakra-ui/react'
 import type { FlexProps } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
 
 import { Link } from '@/components'
-import { NAV_LINKS } from '@/constants'
+import { NAV_LINKS, SOCIAL_LINKS } from '@/constants'
 
 export const MobileMenu: React.FC<FlexProps> = (props) => {
   const { isOpen, onOpen, onClose } = useDisclosure()
@@ -34,14 +35,24 @@ export const MobileMenu: React.FC<FlexProps> = (props) => {
         <DrawerOverlay />
         <DrawerContent>
           <DrawerCloseButton color="fg" />
-          <DrawerBody bg="gray.200" color="gray.900" textAlign="center">
-            <Flex direction="column" mt={16} gap={6}>
+          <DrawerBody display="flex" flexDirection="column" justifyContent="space-between" bg="gray.200" color="gray.900" textAlign="center" pb={8}>
+            <Flex direction="column" my={16} gap={6}>
               <Link href="/" aria-label="Solidity home" mx="auto" onClick={onClose}>
                 <Image src="/assets/solidity-logo.svg" alt="Solidity logo" h="50px" />
               </Link>
               {NAV_LINKS.map(({ name, href }) => (
                 <Link href={href} key={name} fontFamily="mono" onClick={onClose}>
                   {name}
+                </Link>
+              ))}
+            </Flex>
+
+            <Flex gap={2} flexWrap="wrap" justify="center">
+              {SOCIAL_LINKS.map(({ href, Icon }) => (
+                <Link href={href} key={href} hideArrow textDecoration="none">
+                  <Grid placeItems="center" borderRadius="base" bg="bg" color="fg" px={5} py={2} gap={4}>
+                    <Icon size={24} />
+                  </Grid>
                 </Link>
               ))}
             </Flex>
