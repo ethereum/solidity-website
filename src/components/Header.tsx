12c2400000aa91c2084274bea7e702754c714407
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-06-16 17:14:52 -0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,94 @@
-import { Flex, Image } from '@chakra-ui/react'
-import { Link, MobileMenu } from '@/components'
-import { NAV_LINKS } from '@/constants'
+import { Flex, IconButton, useColorMode } from '@chakra-ui/react'
+import { Link, MobileMenu, SolidityLogo } from '@/components'
+import { NAV_LINKS, NAV_HEIGHT } from '@/constants'
+import { motion, useScroll, useTransform } from 'framer-motion'
+import { SunIcon, MoonIcon } from '@chakra-ui/icons'
 
-export const Header: React.FC = () => (
-  <Flex
-    as="header"
-    px={8}
-    py={4}
-    alignItems="center"
-    justify="space-between"
-    position="sticky"
-    top={0}
-    zIndex="sticky"
-    _before={{
-      content: '""',
-      position: 'absolute',
-      inset: 0,
-      bg: 'bg',
-      opacity: 0.95,
-      zIndex: -1,
-    }}
-    backdropFilter="blur(3px)"
-    // boxShadow="md" // TODO: Animate in on scroll
-  >
-    <Link href="/" aria-label="Go home">
-      <Image src="/assets/solidity-logo.svg" alt="Solidity logo" h="50px" />
-    </Link>
-    <Flex as="nav" display={['none', null, 'block']}>
-      {NAV_LINKS.map(({ name, href }) => (
+export const Header: React.FC = () => {
+  const { colorMode, toggleColorMode } = useColorMode()
+  const isDarkMode = colorMode === 'dark'
+  const { scrollY } = useScroll()
+  const motionStyles = {
+    backdrop: {
+      opacity: useTransform(scrollY, [250, 300], [0, 0.95]),
+    },
+    logo: {
+      mt: useTransform(scrollY, [0, 64], [50, 0]),
+      h: useTransform(scrollY, [64, 200], [175, 40]),
+    },
+  }
+  return (
+    <Flex
+      px={8}
+      py={4}
+      justify="space-between"
+      position="sticky"
+      top={0}
+      zIndex="sticky"
+      height="272px" // Full starting height of Nav + Logo
+    >
+      <motion.div
+        style={{
+          position: 'absolute',
+          inset: 0,
+          background: 'var(--chakra-colors-bg)',
+          zIndex: -1,
+          transition: 'background 200ms linear !important',
+          backdropFilter: 'blur(3px)',
+          height: `min(${NAV_HEIGHT}, 100%)`,
+          boxShadow: 'var(--chakra-shadows-md)', 
+          opacity: motionStyles.backdrop.opacity,
+        }}
+      />
+      <motion.div
+        style={{
+          height: motionStyles.logo.h,
+          marginTop: motionStyles.logo.mt,
+        }}
+      >
         <Link
-          key={name}
-          href={href}
-          mx={2}
-          fontFamily="mono"
-          letterSpacing="-0.02em"
+          href="/"
+          aria-label="Go home"
+          display="block"
+          h="100%"
+          w="fit-content"
         >
-          {name}
+          <SolidityLogo
+            height="100%"
+            color="text"
+            preserveAspectRatio="176/113"
+          />
         </Link>
-      ))}
+      </motion.div>
+      <Flex
+        as="nav"
+        display={['none', null, 'flex']}
+        h="fit-content"
+        alignItems="center"
+      >
+        {NAV_LINKS.map(({ name, href }) => (
+          <Link
+            key={name}
+            href={href}
+            px={2}
+            py={1}
+            fontFamily="mono"
+            letterSpacing="-0.02em"
+            textDecoration="none"
+          >
+            {name}
+          </Link>
+        ))}
+        <IconButton
+          aria-label="Toggle light/dark"
+          icon={isDarkMode ? <SunIcon /> : <MoonIcon />}
+          onClick={toggleColorMode}
+          variant="ghost"
+          size="md"
+        />
+      </Flex>
+      {/* Toggle light/dark */}
+      <MobileMenu display={['block', null, 'none']} />
     </Flex>
-    <MobileMenu display={['block', null, 'none']} />
-  </Flex>
-)
+  )
+}
