cc5a2d855dfc7cbbaba6cd1eedb7b2326af647d3
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-06-17 19:09:36 -0700

    update EventPreview and data

--- src/components/EventPreview.tsx
+++ src/components/EventPreview.tsx
@@ -1,60 +1,18 @@
 import { Grid } from '@chakra-ui/react'
 import type { Event } from '@/types'
 import { EventCard } from '@/components'
-
-const events: Event[] = [
-  {
-    title: 'Solidity Summit 2020',
-    location: 'Remote',
-    startDate: 'April 29, 2020',
-    endDate: 'April 30, 2020',
-    links: [
-      {
-        label: 'Event Recap',
-        href: 'https://blog.soliditylang.org/2020/06/09/solidity-summit-recap/',
-      },
-      {
-        label: 'Talks',
-        href: 'https://www.youtube.com/watch?v=lhjo2FuU4v0&list=PLaM7G4Llrb7xlGxwlYGTy1T-GHpytE3RC',
-      },
-    ],
-  },
-  {
-    title: 'Solidity Summit 2022',
-    location: 'Tolhuistuin, Amsterdam',
-    startDate: 'Wed, April 20, 2022',
-    endDate: 'Wed, April 20, 2022',
-    imageSrc: '/assets/solidity-summit-2022.png',
-    links: [
-      {
-        label: 'Event Recap',
-        href: 'https://blog.soliditylang.org/2022/05/03/solidity-summit-2022-recap/',
-      },
-      {
-        label: 'Talks',
-        href: 'https://www.youtube.com/watch?v=6m5EDuCjxgk&list=PLX8x7Zj6Vezl1lqBgxiQH3TFbRNZza8Fk',
-      },
-    ],
-  },
-  {
-    title: 'Underhanded Solidity Contest 2022',
-    location: 'Remote',
-    startDate: 'Wed, April 29, 2022',
-    endDate: 'Wed, April 30, 2022',
-    imageSrc: '/assets/underhanded-solidity-contest-2022.png',
-    links: [
-      {
-        label: 'Event Recap',
-        href: 'https://underhanded.soliditylang.org/',
-      },
-    ],
-  },
-]
+import { events } from '@/data'
 
 export const EventPreview: React.FC = () => (
   <Grid templateColumns={['1fr', null, 'repeat(3, 1fr)']} gap={4}>
-    {events.map((event: Event) => (
-      <EventCard event={event} key={event.title} />
-    ))}
+    {events
+      .filter(
+        (event: Event) =>
+          new Date(event.endDate) <
+          new Date(new Date().setDate(new Date().getDate() + 1))
+      )
+      .map((event: Event) => (
+        <EventCard event={event} key={event.title} />
+      ))}
   </Grid>
 )
---
+++ src/data/events.ts
@@ -0,0 +1,66 @@
+import type { Event } from '../types'
+
+export const events: Event[] = [
+  {
+    title: 'Solidity Summit 2020',
+    location: 'Remote',
+    startDate: 'April 29, 2020',
+    endDate: 'April 30, 2020',
+    links: [
+      {
+        label: 'Event Recap',
+        href: 'https://blog.soliditylang.org/2020/06/09/solidity-summit-recap/',
+      },
+      {
+        label: 'Talks',
+        href: 'https://www.youtube.com/watch?v=lhjo2FuU4v0&list=PLaM7G4Llrb7xlGxwlYGTy1T-GHpytE3RC',
+      },
+    ],
+  },
+  {
+    title: 'Solidity Summit 2022',
+    location: 'Tolhuistuin, Amsterdam',
+    startDate: 'Wed, April 20, 2022',
+    endDate: 'Wed, April 20, 2022',
+    imageSrc: '/assets/solidity-summit-2022.png',
+    links: [
+      {
+        label: 'Event Recap',
+        href: 'https://blog.soliditylang.org/2022/05/03/solidity-summit-2022-recap/',
+      },
+      {
+        label: 'Talks',
+        href: 'https://www.youtube.com/watch?v=6m5EDuCjxgk&list=PLX8x7Zj6Vezl1lqBgxiQH3TFbRNZza8Fk',
+      },
+    ],
+  },
+  {
+    title: 'Underhanded Solidity Contest 2022',
+    location: 'Remote',
+    startDate: 'Wed, April 29, 2022',
+    endDate: 'Wed, April 30, 2022',
+    imageSrc: '/assets/underhanded-solidity-contest-2022.png',
+    links: [
+      {
+        label: 'Event Recap',
+        href: 'https://underhanded.soliditylang.org/',
+      },
+    ],
+  },
+  {
+    title: 'Solidity Summit 2023',
+    location: 'Istanbul, Turkey',
+    startDate: 'November, 2023',
+    endDate: 'November, 2023',
+
+    imageSrc: '/assets/solidity-summit-2022.png',
+    links: [
+      {
+        label: 'Join us',
+        href: 'https://summit.soliditylang.org/',
+      },
+    ],
+  },
+].sort(
+  (a, b) => new Date(b.startDate).getTime() - new Date(a.startDate).getTime()
+)
