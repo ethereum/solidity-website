14cc9c781a572ffe9a73f7bba12f2d9c7ba11e3f
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-06-21 17:38:25 -0700

    fix blog post filtering

--- src/components/BlogPostListSection.tsx
+++ src/components/BlogPostListSection.tsx
@@ -8,71 +8,62 @@ export const BlogPostListSection: React.FC<BlogProps> = ({
   allPostsData,
   page,
   totalPages,
-}) => {
-  const slicedPosts = allPostsData.slice(
-    (page - 1) * MAX_POSTS_PER_PAGE,
-    page * MAX_POSTS_PER_PAGE
-  )
-  return (
-    <Section
-      direction="column"
-      gap={8}
-      maxW="container.lg"
-      pt={28}
-      pb={64}
-      fontSize="md"
-      mx="auto"
-    >
-      {slicedPosts.map(({ frontmatter, content, url }) => (
-        <BlogPostPreview
-          key={url}
-          frontmatter={frontmatter}
-          content={content}
-          url={url}
-        />
-      ))}
-      <Flex justify="center" gap={3}>
-        {page > 1 ? (
-          <ButtonLink
-            href={page === 2 ? BLOG_PATH : `${BLOG_PAGE_PATH}/${page - 1}`}
+}) => (
+  <Section
+    direction="column"
+    gap={8}
+    maxW="container.lg"
+    pt={28}
+    pb={64}
+    fontSize="md"
+    mx="auto"
+  >
+    {allPostsData.map(({ frontmatter, content, url }) => (
+      <BlogPostPreview
+        key={url}
+        frontmatter={frontmatter}
+        content={content}
+        url={url} />
+    ))}
+    <Flex justify="space-between" gap={3}>
+      {page > 1 ? (
+        <ButtonLink
+          href={page === 2 ? BLOG_PATH : `${BLOG_PAGE_PATH}/${page - 1}`}
+        >
+          <Icon
+            verticalAlign="middle"
+            as={FaArrowLeft}
+            size={16}
+            _groupHover={{ color: 'primary' }} />
+
+          <Text
+            as="span"
+            marginStart={3}
+            fontFamily="heading"
+            textTransform="uppercase"
           >
-            <Icon
-              verticalAlign="middle"
-              as={FaArrowLeft}
-              size={16}
-              _groupHover={{ color: 'primary' }}
-            />
+            Newer posts
+          </Text>
+        </ButtonLink>
+      ) : <Spacer />}
+      {page < totalPages ? (
+        <ButtonLink href={`${BLOG_PAGE_PATH}/${page + 1}`}>
+          <Icon
+            verticalAlign="middle"
+            as={FaArrowRight}
+            size={16}
+            _groupHover={{ color: 'primary' }} />
 
-            <Text
-              as="span"
-              marginStart={3}
-              fontFamily="heading"
-              textTransform="uppercase"
-            >
-              Newer posts
-            </Text>
-          </ButtonLink>
-        ): <Spacer />}
-        {page < totalPages ? (
-          <ButtonLink href={`${BLOG_PAGE_PATH}/${page + 1}`}>
-            <Icon
-              verticalAlign="middle"
-              as={FaArrowRight}
-              size={16}
-              _groupHover={{ color: 'primary' }}
-            />
-
-            <Text
-              as="span"
-              marginStart={3}
-              fontFamily="heading"
-              textTransform="uppercase"
-            >
-              Older posts
-            </Text>
-          </ButtonLink>
-        ) : <Spacer />}
-      </Flex>
-    </Section>
-  )
-}
+          <Text
+            as="span"
+            marginStart={3}
+            fontFamily="heading"
+            textTransform="uppercase"
+          >
+            Older posts
+          </Text>
+        </ButtonLink>
+      ) : <Spacer />}
+    </Flex>
+  </Section>
+)
--- src/utils/getPostsDataForPage.ts
+++ src/utils/getPostsDataForPage.ts
@@ -5,28 +5,23 @@ import { BLOG_DIR, MATTER_OPTIONS, BLOG_PATH } from '@/constants'
 import type { BlogPostProps } from '@/interfaces'
 
 export const getPostsDataForPage = (
-  files: string[],
+  sortedFiles: string[],
   page: number,
   fs: any
 ): BlogPostProps[] =>
-  files
-    .filter((_, i) => i >= (page - 1) * 10 && i < page * 10)
-    .map((file) => {
-      // Read markdown file as string
-      const fullPath = path.join(BLOG_DIR, file)
-      const fileContents = fs.readFileSync(fullPath, 'utf8')
-      // Use gray-matter to parse the post metadata section
-      const { data: frontmatter, content } = matter(
-        fileContents,
-        MATTER_OPTIONS
-      )
-      const params = getPostParamsFromFilename(file)
-      const url = params
-        ? `${BLOG_PATH}/${params.YYYY}/${params.MM}/${params.DD}/${params.post}`
-        : ''
-      return {
-        frontmatter,
-        content: getSlicedContent(content),
-        url,
-      } as BlogPostProps
-    })
+  sortedFiles.slice((page - 1) * 10, page * 10).map((file) => {
+    // Read markdown file as string
+    const fullPath = path.join(BLOG_DIR, file)
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
+    // Use gray-matter to parse the post metadata section
+    const { data: frontmatter, content } = matter(fileContents, MATTER_OPTIONS)
+    const params = getPostParamsFromFilename(file)
+    const url = params
+      ? `${BLOG_PATH}/${params.YYYY}/${params.MM}/${params.DD}/${params.post}`
+      : ''
+    return {
+      frontmatter,
+      content: getSlicedContent(content),
+      url,
+    } as BlogPostProps
+  })
