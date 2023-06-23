9f058ab047f4f0c676f328918b42d4f8381cf1ee
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-06-22 22:09:53 -0700

    update mobile menu to design

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -6,13 +6,13 @@ import { Link } from '@/components'
 export const SocialLinks: React.FC<FlexProps> = (props) => (
   <Flex
     direction={{ base: 'column', sm: 'row' }}
-    gap={4}
+    columnGap={8}
     flexWrap="wrap"
     justify="center"
     {...props}
   >
     {SOCIAL_LINKS.map(({ name, href, Icon }) => (
-      <Link href={href} key={name} hideArrow textDecoration="none" px={2}>
+      <Link href={href} key={name} hideArrow textDecoration="none">
         <Flex alignItems="center" gap={2}>
           <Box borderRadius="base" py={2}>
             <Icon size={18} />
