e23f119f8cf71b0f09f03fdb37e18784b998c713
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-06-29 16:40:53 -0700

    preload fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,26 @@ import { Html, Head, Main, NextScript } from 'next/document'
 import theme from '../theme'
 
 export default function Document() {
+  const preloadFonts = [
+    "overpass/overpass-regular.otf",
+    "overpass/overpass-bold.otf",
+    "overpass-mono/overpass-mono-regular.otf",
+    "overpass-mono/overpass-mono-bold.otf",
+  ];
+
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {preloadFonts.map((font) => (
+          <link
+            key={font}
+            rel="preload"
+            href={`/fonts/${font}`}
+            as="font"
+            crossOrigin=""
+          />
+        ))}
+      </Head>
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
