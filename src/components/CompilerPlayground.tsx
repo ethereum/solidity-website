c101b5ad5ff951c90f12f39226c59d5f72f85853
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-06-30 13:40:14 -0700

    implement web worker for compiler

--- src/components/CompilerPlayground.tsx
+++ src/components/CompilerPlayground.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react'
 import dynamic from 'next/dynamic'
 import {
-  Box,
   Flex,
   FlexProps,
   Grid,
   Select,
   Text,
+  Textarea,
+  TextareaProps,
+  useBreakpointValue,
   useColorMode,
 } from '@chakra-ui/react'
 import '@uiw/react-textarea-code-editor/dist.css'
@@ -17,39 +19,117 @@ const CodeEditor = dynamic(
   { ssr: false }
 )
 
+const ResultTextarea: React.FC<TextareaProps> = (props) => (<Textarea
+  readOnly
+  flex={1}
+  resize="vertical"
+  fontSize="xs"
+  p={2}
+  {...props}
+/>)
+interface CompilerResult {
+  evm: {
+    assembly: string
+    bytecode: { object: string }
+    gasEstimates: { creation: { totalCost: string } }
+  }
+}
+
+interface CompilerResultState {
+  assembly: string
+  bytecode: string
+  totalCost: string
+  version: string
+  contractName: string
+  bytes?: string
+}
+
 export const CompilerPlayground: React.FC<FlexProps> = (props) => {
-  const [code, setCode] = useState(examples.get('helloWorld'))
   const { colorMode } = useColorMode()
+  const [worker, setWorker] = useState<Worker | null>(null)
+  const [code, setCode] = useState<string>(examples.get('helloWorld'))
+  const [compilerResults, setCompilerResults] = useState<CompilerResultState>({
+    assembly: '',
+    bytecode: '',
+    totalCost: '',
+    contractName: '',
+    version: '',
+  })
+  const { assembly, bytecode, totalCost, version, contractName } = compilerResults
+  const bytes: number = bytecode.length / 2
   useEffect(() => {
-    document?.documentElement.setAttribute('data-color-mode', 'dark')
+    // Guard for undefined Worker type
+    if (typeof Worker === 'undefined') return
+    // Instantiate a new worker, based on `bundle.js` file
+    // See: https://github.com/r0qs/solcjs-webworker-example
+    const newWorker = new Worker('./bundle.js')
+    // Listen for messages FROM the worker, with compiler results
+    newWorker.addEventListener(
+      'message',
+      function (e: MessageEvent) {
+        console.log({ e })
+        const { version: _version }: { version: string } = e.data
+        const contract: { [key: string]: CompilerResult } =
+          e.data.output.contracts['']
+        const [_contractName, result]: [string, CompilerResult] =
+          Object.entries(contract)[0]
+        const {
+          evm: {
+            assembly: _assembly,
+            bytecode: { object: _bytecode },
+            gasEstimates: {
+              creation: { totalCost: _totalCost },
+            },
+          },
+        } = result
+        setCompilerResults({
+          assembly: _assembly,
+          bytecode: _bytecode,
+          totalCost: _totalCost,
+          contractName: _contractName,
+          version: _version,
+        })
+      },
+      false
+    )
+    setWorker(newWorker)
   }, [])
+
+  useEffect(() => {
+    // Guard for null worker
+    if (!worker) return
+    // Every time `code` changes, post a message TO the worker
+    worker.postMessage({ contractCode: code })
+  }, [worker, code])
+
   const onSelectionChange = (e: any) => {
     if (!e?.target?.value) return
     setCode(examples.get(e.target.value))
   }
-  // <Script src="mode-javascript.js" type="text/javascript" />
-  // <Script src="mode-solidity.js" type="text/javascript" />
-  // <Script src="compiler.js" type="text/javascript" />
+  const handleCodeChange = (evn: any) => {
+    setCode(evn.target.value)
+  }
+  const editorHeight = useBreakpointValue({ base: '300px', md: '500px'})
   return (
     <Flex direction="column" w="full" gap={2} {...props}>
-      <Select variant="outline" onChange={onSelectionChange}>
+      <Select bg="mode" onChange={onSelectionChange} w="fit-content">
         <option value="helloWorld">Hello World!</option>
         <option value="erc20">ERC20</option>
         <option value="simpleAuction">Simple Auction</option>
       </Select>
 
-      <Grid templateColumns={['1fr', null, 'repeat(2, 1fr)']}>
+      <Grid templateColumns={{ base: '1fr', md: 'repeat(2, 1fr)' }} gap={4}>
         <CodeEditor
           value={code}
           language="sol"
           placeholder="pragma solidity ^0.8.20;"
-          onChange={(evn) => setCode(evn.target.value)}
+          onChange={handleCodeChange}
           data-color-mode={colorMode}
           padding={15}
           style={{
             fontSize: 12,
-            backgroundColor: '#f5f5f5',
-            height: '400px',
+            backgroundColor: 'var(--chakra-colors-mode)',
+            height: editorHeight,
             overflowY: 'scroll',
             resize: 'vertical',
             fontFamily:
@@ -61,18 +141,34 @@ export const CompilerPlayground: React.FC<FlexProps> = (props) => {
           direction="column"
           gap={2}
           flex={1}
-          p={2}
-          h={['20rem', '25rem', '30rem']}
+          maxW="100%"
+          textOverflow="ellipsis"
         >
-          <Text fontWeight="bold">Compiler result</Text>
+          <Text fontWeight="bold" fontSize="lg">Compiler result</Text>
+
           <Text fontWeight="bold">
-            Compiler version
-            <br />
-            <Text as="span" id="compiler_version" fontWeight="normal">
-              ...
+            Compiler version:{' '}
+            <Text as="span" fontWeight="normal">
+              {version}
             </Text>
           </Text>
-          <Box id="compiler_errors" w="full" h="100px" />
+          
+          <Text fontWeight="bold">
+            {contractName} ({bytes} bytes)
+          </Text>
+
+          <Text>Deployment costs: {totalCost} gas</Text>
+
+          <Text fontWeight="bold">
+            Bytecode
+          </Text>
+          <ResultTextarea value={bytecode} />
+
+          <Text fontWeight="bold">
+            Assembly
+          </Text>
+          <ResultTextarea value={assembly} />
+
         </Flex>
       </Grid>
     </Flex>
