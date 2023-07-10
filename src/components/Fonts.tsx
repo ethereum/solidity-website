ef4ae65c27897c7d337649d81f4f85ae01907bac
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-07-10 13:50:37 -0700

    use cdnfonts import for overpass fonts
    possibly temporary; relocating font assets for now and removing those not needed at all

--- src/components/Fonts.tsx
+++ src/components/Fonts.tsx
@@ -4,7 +4,8 @@ import { Global } from '@emotion/react'
 export const Fonts = () => (
   <Global
     styles={`
-      @import url('https://fonts.googleapis.com/css?family=Overpass:400,400i,700,700i|Overpass+Mono:400,400i,700,700i&display=swap');
+      @import url("https://fonts.cdnfonts.com/css/overpass");
+      @import url("https://fonts.cdnfonts.com/css/overpass-mono");
     `}
   />
 )
