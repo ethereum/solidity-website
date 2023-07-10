795ec70d32a55015852e9c71ba548de9b2ef3172
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-07-10 12:07:51 -0700

    load fonts from g api

--- src/components/Fonts.tsx
+++ src/components/Fonts.tsx
@@ -4,56 +4,7 @@ import { Global } from '@emotion/react'
 export const Fonts = () => (
   <Global
     styles={`
-      /* latin */
-      @font-face {
-        font-family: 'Overpass';
-        font-style: light;
-        font-weight: 300;
-        font-display: swap;
-        src: url("/fonts/overpass/overpass-light.otf");
-      }
-      @font-face {
-        font-family: 'Overpass';
-        font-style:  normal;
-        font-weight: 400;
-        font-display: swap;
-        src: url("/fonts/overpass/overpass-regular.otf");
-      }
-      @font-face {
-        font-family: 'Overpass';
-        font-style:  medium;
-        font-weight: 500;
-        font-display: swap;
-        src: url("/fonts/overpass/overpass-semibold.otf");
-      }
-      @font-face {
-        font-family: 'Overpass';
-        font-style:  bold;
-        font-weight: 700;
-        font-display: swap;
-        src: url("/fonts/overpass/overpass-bold.otf");
-      }
-      @font-face {
-        font-family: 'Overpass';
-        font-style:  italic;
-        font-weight: 400;
-        font-display: swap;
-        src: url("/fonts/overpass/overpass-italic.otf");
-      }
-      @font-face {
-        font-family: 'Overpass Mono';
-        font-style:  regular;
-        font-weight: 400;
-        font-display: swap;
-        src: url("/fonts/overpass-mono/overpass-mono-regular.otf");
-      }
-      @font-face {
-        font-family: 'Overpass Mono';
-        font-style:  bold;
-        font-weight: 700;
-        font-display: swap;
-        src: url("/fonts/overpass-mono/overpass-mono-bold.otf");
-      }
+      @import url('https://fonts.googleapis.com/css?family=Overpass:400,400i,700,700i|Overpass+Mono:400,400i,700,700i&display=swap');
     `}
   />
 )
