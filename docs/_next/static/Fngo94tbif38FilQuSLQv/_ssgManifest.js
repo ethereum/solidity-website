112080250dccd4f2ecc1254c8ee603e197075028
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-07-11 13:11:22 -0700

    export static nextjs build to docs for gh pages

---
+++ docs/_next/static/Fngo94tbif38FilQuSLQv/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fblog","\u002Fblog\u002F[YYYY]\u002F[MM]\u002F[DD]\u002F[post]","\u002Fblog\u002Fcategory\u002F[category]","\u002Fblog\u002Fpage\u002F[page]","\u002Fevent\u002F[event]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
