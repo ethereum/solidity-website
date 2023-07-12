a1e82590fafd1ca90dc2392f5cd34deef8601a51
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-07-12 13:52:28 -0700

    de-couple array of content to react elements

--- src/components/ContributingCards.tsx
+++ src/components/ContributingCards.tsx
@@ -1,39 +1,17 @@
-import {
-  Grid,
-  type GridProps,
-  Text,
-  Divider,
-  useBreakpointValue,
-} from '@chakra-ui/react'
-import { Fragment } from 'react'
+import { Grid, type GridProps, Text, Divider } from '@chakra-ui/react'
+import { Link } from '@/components'
 
-interface ContributingCard {
-  title: string
-  description: string
-}
-const cards: ContributingCard[] = [
-  {
-    title: 'Reporting issues and vulnerabilities',
-    description:
-      'To report an issue, please use the GitHub issues tracker https://github.com/ethereum/solidity/issues/new?assignees=&labels=bug+%3Abug%3A&projects=&template=bug_report.md&title=. To report a vulnerability, please check out the instructions in the SECURITY.md https://github.com/ethereum/solidity/blob/develop/SECURITY.md#reporting-a-vulnerability.',
-  },
-  {
-    title: 'Translating the documentation',
-    description:
-      'Translations help developers from all corners of the world to be able to read the documentation and learn Solidity.',
-  },
-  {
-    title: 'Fixing and responding to issues',
-    description:
-      'Fixing and responding to issues, especially those tagged as “good first issue”, is a great way to get started for external contributors.',
-  },
-  {
-    title: 'Contributing to language design',
-    description:
-      'We welcome Solidity power users, auditors, security experts and tooling developers to get involved in the Solidity language design process. >Join the <a href="https://forum.soliditylang.org/">Solidity forum</a>, where existing properties of the language and proposals for new language features can be discussed.',
-  },
-]
 export const ContributingCards: React.FC<GridProps> = (props) => {
+  const getGridColumn = (i: number) => ({
+    base: `1 / 2`,
+    md: `${(i % 2) + 1} / ${(i % 2) + 2}`,
+    lg: `${(i % 4) + 1} / ${(i % 4) + 2}`,
+  })
+  const getGridRow = (i: number, n: number) => ({
+    base: 'auto',
+    md: `${Math.floor(i / 2) * 3 + n}`,
+    lg: `${Math.floor(i / 4) * 3 + n}`,
+  })
   return (
     <Grid
       templateColumns={{
@@ -44,49 +22,131 @@ export const ContributingCards: React.FC<GridProps> = (props) => {
       columnGap={8}
       {...props}
     >
-      {cards.map(({ title, description }, i) => {
-        const gridColumn = {
-          base: `1 / 2`,
-          md: `${(i % 2) + 1} / ${(i % 2) + 2}`,
-          lg: `${(i % 4) + 1} / ${(i % 4) + 2}`,
-        }
-        const getGridRow = (i: number, n: number) => ({
-          base: 'auto',
-          md: `${Math.floor(i / 2) * 3 + n}`,
-          lg: `${Math.floor(i / 4) * 3 + n}`,
-        })
-        return (
-          <Fragment key={title}>
-            <Text
-              textStyle="h6-mono"
-              color="secondary"
-              gridColumn={gridColumn}
-              gridRow={getGridRow(i, 1)}
-              alignSelf="end"
-            >
-              {title}
-            </Text>
-            <Divider
-              borderColor="highlight"
-              gridColumn={gridColumn}
-              gridRow={getGridRow(i, 2)}
-            />
-            <Text
-              gridColumn={gridColumn}
-              gridRow={getGridRow(i, 3)}
-              mt={4}
-              mb={{ base: '12 !important', lg: '4 !important' }}
-              sx={{
-                '&:last-of-type': {
-                  mb: '0 !important',
-                },
-              }}
-            >
-              {description}
-            </Text>
-          </Fragment>
-        )
-      })}
+      <Text
+        textStyle="h6-mono"
+        color="secondary"
+        gridColumn={getGridColumn(0)}
+        gridRow={getGridRow(0, 1)}
+        alignSelf="end"
+      >
+        Reporting issues and vulnerabilities
+      </Text>
+      <Divider
+        borderColor="highlight"
+        gridColumn={getGridColumn(0)}
+        gridRow={getGridRow(0, 2)}
+      />
+      <Text
+        gridColumn={getGridColumn(0)}
+        gridRow={getGridRow(0, 3)}
+        mt={4}
+        mb={{ base: '12 !important', lg: '4 !important' }}
+        sx={{
+          '&:last-of-type': {
+            mb: '0 !important',
+          },
+        }}
+      >
+        To report an issue, please use the{' '}
+        <Link href="https://github.com/ethereum/solidity/issues/new?assignees=&labels=bug+%3Abug%3A&projects=&template=bug_report.md&title=">
+          GitHub issues tracker
+        </Link>
+        . To report a vulnerability, please check out the instructions in the{' '}
+        <Link href="https://github.com/ethereum/solidity/blob/develop/SECURITY.md#reporting-a-vulnerability">
+          SECURITY.md
+        </Link>
+        .
+      </Text>
+
+      <Text
+        textStyle="h6-mono"
+        color="secondary"
+        gridColumn={getGridColumn(1)}
+        gridRow={getGridRow(1, 1)}
+        alignSelf="end"
+      >
+        Translating the documentation
+      </Text>
+      <Divider
+        borderColor="highlight"
+        gridColumn={getGridColumn(1)}
+        gridRow={getGridRow(1, 2)}
+      />
+      <Text
+        gridColumn={getGridColumn(1)}
+        gridRow={getGridRow(1, 3)}
+        mt={4}
+        mb={{ base: '12 !important', lg: '4 !important' }}
+        sx={{
+          '&:last-of-type': {
+            mb: '0 !important',
+          },
+        }}
+      >
+        Translations help developers from all corners of the world to be able to
+        read the documentation and learn Solidity.
+      </Text>
+
+      <Text
+        textStyle="h6-mono"
+        color="secondary"
+        gridColumn={getGridColumn(2)}
+        gridRow={getGridRow(2, 1)}
+        alignSelf="end"
+      >
+        Fixing and responding to issues
+      </Text>
+      <Divider
+        borderColor="highlight"
+        gridColumn={getGridColumn(2)}
+        gridRow={getGridRow(2, 2)}
+      />
+      <Text
+        gridColumn={getGridColumn(2)}
+        gridRow={getGridRow(2, 3)}
+        mt={4}
+        mb={{ base: '12 !important', lg: '4 !important' }}
+        sx={{
+          '&:last-of-type': {
+            mb: '0 !important',
+          },
+        }}
+      >
+        Fixing and responding to issues, especially those tagged as “good first
+        issue”, is a great way to get started for external contributors.
+      </Text>
+
+      <Text
+        textStyle="h6-mono"
+        color="secondary"
+        gridColumn={getGridColumn(3)}
+        gridRow={getGridRow(3, 1)}
+        alignSelf="end"
+      >
+        Contributing to language design
+      </Text>
+      <Divider
+        borderColor="highlight"
+        gridColumn={getGridColumn(3)}
+        gridRow={getGridRow(3, 2)}
+      />
+      <Text
+        gridColumn={getGridColumn(3)}
+        gridRow={getGridRow(3, 3)}
+        mt={4}
+        mb={{ base: '12 !important', lg: '4 !important' }}
+        sx={{
+          '&:last-of-type': {
+            mb: '0 !important',
+          },
+        }}
+      >
+        We welcome Solidity power users, auditors, security experts and tooling
+        developers to get involved in the Solidity language design process. Join
+        the <Link href="https://forum.soliditylang.org/">Solidity forum</Link>,
+        where existing properties of the language and proposals for new language
+        features can be discussed.
+      </Text>
     </Grid>
   )
 }
