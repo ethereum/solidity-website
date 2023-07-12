a6bbce0a121f16204269b85d33100e387d115731
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-07-11 19:59:06 -0700

    trailingSlash: true

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
   experimental: { esmExternals: true },
+  trailingSlash: true,
 }
 
 const removeImports = require('next-remove-imports')()
