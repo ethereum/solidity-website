f827e2d3f47103012a96336575415cba943de9ad
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-07-12 17:11:49 -0700

    Sort events by endDate on fetch

--- src/utils/getAllEvents.ts
+++ src/utils/getAllEvents.ts
@@ -6,17 +6,30 @@ import type { EventPost } from '@/interfaces'
 
 export const getAllEvents = (fs: any): EventPost[] => {
   const eventFiles: string[] = fs.readdirSync(EVENTS_DIR)
-  return eventFiles.map((file) => {
-    // Read markdown file as string
-    const fullPath = path.join(EVENTS_DIR, file)
-    const fileContents = fs.readFileSync(fullPath, 'utf8')
-    // Use gray-matter to parse the post metadata section
-    const { data: frontmatter, content } = matter(fileContents, MATTER_OPTIONS)
-    const url: string = `${EVENTS_PATH}/${file.replace(/\.md$/, '')}`
-    return {
-      frontmatter,
-      content: sanitizePostPreviewContent(content),
-      url,
-    } as EventPost
-  })
+  return (
+    eventFiles
+      .map((file) => {
+        // Read markdown file as string
+        const fullPath = path.join(EVENTS_DIR, file)
+        const fileContents = fs.readFileSync(fullPath, 'utf8')
+        // Use gray-matter to parse the post metadata section
+        const { data: frontmatter, content } = matter(
+          fileContents,
+          MATTER_OPTIONS
+        )
+        const url: string = `${EVENTS_PATH}/${file.replace(/\.md$/, '')}`
+        return {
+          frontmatter,
+          content: sanitizePostPreviewContent(content),
+          url,
+        } as EventPost
+      })
+      // Sort newest first
+      .sort((a, b) =>
+        new Date(a.frontmatter.endDate).getTime() >
+        new Date(b.frontmatter.endDate).getTime()
+          ? -1
+          : 1
+      )
+  )
 }
