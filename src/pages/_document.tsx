00796c5dabdc8a99a605aad9a4dcdd96211b6941
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-07-22 16:18:08 -0400

    remove font preloads from _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,26 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 import theme from '../theme'
 
 export default function Document() {
-  const preloadFonts = [
-    'overpass/overpass-regular.otf',
-    'overpass/overpass-bold.otf',
-    'overpass-mono/overpass-mono-regular.otf',
-    'overpass-mono/overpass-mono-bold.otf',
-  ]
-
   return (
     <Html lang="en">
-      <Head>
-        {preloadFonts.map((font) => (
-          <link
-            key={font}
-            rel="preload"
-            href={`/fonts/${font}`}
-            as="font"
-            crossOrigin=""
-          />
-        ))}
-      </Head>
+      <Head />
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
