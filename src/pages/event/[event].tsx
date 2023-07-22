4a398980e596737da38a530bae36c35a2c94b200
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-07-22 15:11:41 -0400

    refactor [event], rm EventListing

--- src/pages/event/[event].tsx
+++ src/pages/event/[event].tsx
@@ -1,11 +1,16 @@
 import fs from 'fs'
 import matter from 'gray-matter'
-import type { GetStaticPaths, GetStaticProps } from 'next/types'
-import { EVENTS_DIR, MAIN_CONTENT_ID, MATTER_OPTIONS } from '@/constants'
+import gfm from 'remark-gfm'
 import { ParsedUrlQuery } from 'querystring'
+import ReactMarkdown from 'react-markdown'
+import ChakraUIRenderer from 'chakra-ui-markdown-renderer'
+import type { GetStaticPaths, GetStaticProps } from 'next/types'
+import { Box, Image, Text } from '@chakra-ui/react'
+import { PageMetadata, YouTube } from '@/components'
+import { EVENTS_DIR, MAIN_CONTENT_ID, MATTER_OPTIONS } from '@/constants'
+import { MDXStyles } from '@/styles'
 import type { EventPost } from '@/interfaces'
-import { EventListing, Hero, PageMetadata } from '@/components'
-import { Box } from '@chakra-ui/react'
+import { formatDate } from '@/utils'
 
 // Generate the paths for each event
 export const getStaticPaths: GetStaticPaths = () => {
@@ -17,14 +22,13 @@ export const getStaticPaths: GetStaticPaths = () => {
   }
   const paths: EventPath[] = []
 
-  // TODO: Uncomment when ready to build these paths as markdown pages
-  // // Get list of all files from our events directory
-  // const files = fs.readdirSync(EVENTS_DIR)
-  // // Generate a path for each one
-  // files.forEach((file) => {
-  //   const fileName = file.replace('.md', '')
-  //   paths.push({ params: { event: fileName } })
-  // })
+  // Get list of all files from our events directory
+  const files = fs.readdirSync(EVENTS_DIR)
+  // Generate a path for each one
+  files.forEach((file) => {
+    const fileName = file.replace('.md', '')
+    paths.push({ params: { event: fileName } })
+  })
 
   // Return list of paths
   return { paths, fallback: false }
@@ -39,16 +43,50 @@ export const getStaticProps: GetStaticProps = async (context) => {
 }
 
 const EventPage: React.FC<EventPost> = ({ frontmatter, content }) => {
-  const { title, location, startDate, endDate } = frontmatter
-  const description = `${location} - ${new Date(
-    startDate
-  ).toLocaleDateString()} - ${new Date(endDate).toLocaleDateString()}`
+  const { title, location, startDate, endDate, imageSrc, youtube } = frontmatter
+  const description = `${location} - ${formatDate(startDate)} - ${formatDate(
+    endDate
+  )}`
+  const isMultiDay = formatDate(startDate) !== formatDate(endDate)
+
   return (
     <>
       <PageMetadata title={title} description={description} />
       <Box as="main" id={MAIN_CONTENT_ID}>
-        <Hero header={title}>{location}</Hero>
-        <EventListing frontmatter={frontmatter} content={content} />
+        {/* Hero image */}
+        <Image
+          src={imageSrc}
+          alt="Event hero image"
+          maxH="450px"
+          w="full"
+          objectFit="cover"
+          mx="auto"
+        />
+        {/* Event hero */}
+        <Box textAlign="center" mt={8}>
+          <Text>
+            {isMultiDay
+              ? `${formatDate(startDate)} - {formatDate(endDate)}`
+              : formatDate(startDate, true)}
+          </Text>
+          <Text as="h1" textStyle="h2-mono" textAlign="center" mt={2} mb={4}>
+            {title}
+          </Text>
+          <Text textStyle="h5" color="text">
+            {location}
+          </Text>
+        </Box>
+        {/* TODO: Embed MAP */}
+        {/* Markdown content */}
+        <Box as="article" px={6} maxW="container.lg" mx="auto" my={16}>
+          <ReactMarkdown
+            components={ChakraUIRenderer(MDXStyles)}
+            remarkPlugins={[gfm]}
+          >
+            {content}
+          </ReactMarkdown>
+        </Box>
+        {youtube && <YouTube url={youtube} />}
       </Box>
     </>
   )
