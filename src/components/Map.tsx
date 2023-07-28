3d8f37feb5c51f4c7a41215bb96cae17f5590dab
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-07-28 07:11:11 -0400

    implement map dark mode

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,4 +1,4 @@
-import { Box, type BoxProps } from '@chakra-ui/react'
+import { Box, useColorModeValue, type BoxProps } from '@chakra-ui/react'
 import dynamic from 'next/dynamic'
 import { Coordinates } from '@/interfaces'
 
@@ -11,8 +11,22 @@ interface MapProps extends BoxProps {
   coordsOverride?: Coordinates | null
   location: string
 }
-export const Map: React.FC<MapProps> = ({ coordsOverride, location, ...boxProps }) => (
-  <Box as="figure" sx={{ '&>div': { w: 'full', h: 'full' } }} {...boxProps}>
-    <OpenStreetMap coordsOverride={coordsOverride} location={location} />
-  </Box>
-)
+export const Map: React.FC<MapProps> = ({
+  coordsOverride,
+  location,
+  ...boxProps
+}) => {
+  const filter = useColorModeValue('', 'invert(100%) hue-rotate(180deg) brightness(1.5)')
+  return (
+    <Box
+      as="figure"
+      sx={{
+        '&>div': { w: 'full', h: 'full', filter },
+        '&>div svg': { filter }
+      }}
+      {...boxProps}
+    >
+      <OpenStreetMap coordsOverride={coordsOverride} location={location} />
+    </Box>
+  )
+}
